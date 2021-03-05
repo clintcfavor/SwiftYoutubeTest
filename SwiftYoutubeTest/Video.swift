561903dd6ba785ca248d17ad4fb6534c70afb380
Clint Favor <clintfavor@Clints-Mac-mini.local>
2021-03-05 14:46:15 +0800

    *Modified gitignore *Added additional files

---
+++ SwiftYoutubeTest/Video.swift
@@ -0,0 +1,16 @@
+//
+//  Video.swift
+//  SwiftYoutubeTest
+//
+//  Created by Clint Favor on 3/5/21.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var publishedDate = ""
+}
